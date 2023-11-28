07be99b21692d18514370b808e4aad2d9af7db26
Emerson Alexandre de oliveira Correa <110613926+emersonocorrea@users.noreply.github.com>
2023-11-28 19:16:13 -0300

    update

---
+++ src/scripts/tailwind.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'outfit': ['Outfit', 'sans-serif']
+        },
+      extend: {
+        colors: {
+          softBlue: '#8BACD9',
+          cyan: '#00FFF8',
+          mainBg: '#0D192C',
+          cardBg: '#15263F',
+          veryDarkBlue: '#2E405A',
+        }
+      }
+    }
+  }
